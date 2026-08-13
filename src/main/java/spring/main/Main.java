cffe912f36e8ff006999539429d31bcc50cb0411
piyushkumar3011141 <piyush3011141@gmail.com>
2026-08-13 22:11:18 +0530

    Initial commit

---
+++ src/main/java/spring/main/Main.java
@@ -0,0 +1,23 @@
+package spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.model.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		
+		Employee bean1 = ioc.getBean("emp1",Employee.class);
+		System.out.println(bean1);
+		
+		
+		Employee bean2 = ioc.getBean("emp2",Employee.class);
+		System.out.println(bean2);
+		
+	}
+
+}
---
+++ src/main/java/spring/model/Employee.java
@@ -0,0 +1,21 @@
+package spring.model;
+
+import java.util.List;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Employee {
+
+	private int id;
+	private String name, gender;
+
+	
+
+}
